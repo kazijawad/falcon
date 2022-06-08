df9aad6741a8c55a20bec1c4bf17787d563e777a
Kazi Jawad <kazi.a.jawad@gmail.com>
2022-06-08 14:08:13 -0700

    test refraction

--- GLTutorial/Programs/model.fs
+++ GLTutorial/Programs/model.fs
@@ -9,7 +9,8 @@ uniform vec3 cameraPosition;
 uniform samplerCube skybox;
 
 void main() {
+    float ratio = 1.00 / 1.52;
     vec3 I = normalize(vPosition - cameraPosition);
-    vec3 R = reflect(I, normalize(vNormal));
+    vec3 R = refract(I, normalize(vNormal), ratio);
     color = vec4(texture(skybox, R).rgb, 1.0);
 }
