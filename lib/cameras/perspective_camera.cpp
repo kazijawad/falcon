f72ffd6ea1fdac22f373cfbd9ff87581cc697953
Kazi Jawad <kazi.a.jawad@gmail.com>
2023-01-22 19:06:05 -0500

    add note on perspective projection

--- lib/cameras/perspective_camera.cpp
+++ lib/cameras/perspective_camera.cpp
@@ -10,28 +10,36 @@ PerspectiveCamera::PerspectiveCamera(float fov, float aspect, float near, float 
     isDirty = true;
 }
 
-float PerspectiveCamera::fov() { return state.fov; }
+float PerspectiveCamera::fov() {
+    return state.fov;
+}
 
 void PerspectiveCamera::setFOV(float v) {
     state.fov = v;
     isDirty = true;
 }
 
-float PerspectiveCamera::aspectRatio() { return state.aspect; }
+float PerspectiveCamera::aspectRatio() {
+    return state.aspect;
+}
 
 void PerspectiveCamera::setAspectRatio(float v) {
     state.aspect = v;
     isDirty = true;
 }
 
-float PerspectiveCamera::near() { return state.near; }
+float PerspectiveCamera::near() {
+    return state.near;
+}
 
 void PerspectiveCamera::setNear(float v) {
     state.near = v;
     isDirty = true;
 }
 
-float PerspectiveCamera::far() { return state.far; }
+float PerspectiveCamera::far() {
+    return state.far;
+}
 
 void PerspectiveCamera::setFar(float v) {
     state.far = v;
@@ -39,6 +47,16 @@ void PerspectiveCamera::setFar(float v) {
 }
 
 void PerspectiveCamera::updateProjectionMatrix() {
+    // The perspective projection is similar to the orthographic projection in
+    // that there's a scaled view volume that's transformed to the canonical
+    // view volume. However, a perspective projection involves field of view (fov)
+    // information that influences the shape of the view volume. It's also more
+    // intuitive for the user to define the frustum in terms of fov and aspect
+    // ratio instead of the view bounds. With this information, a perspective
+    // divide is performed of the (x, y, z) coordinates against the homogenous
+    // coordinate, w. This division can also affect depth buffer accuracy because
+    // the z coordinate is divided against w. The accuracy degradation is noticeable
+    // for near planes close to zero or extremely large far planes.
     projectionMatrix = glm::perspective(glm::radians(state.fov), state.aspect, state.near, state.far);
 }
 
